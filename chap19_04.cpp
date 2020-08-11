e4f44da696ee855994e462c2533e73bc44af0c37
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-08-11 10:25:09 +0900

    create file

---
+++ chap19_04.cpp
@@ -0,0 +1,44 @@
+#include "all.h"
+
+// class定義
+struct TestClass{
+
+    int a;
+
+    // コンストラクタ
+    TestClass(int x){
+        a = x;
+    }
+
+    // デストラクタ
+    ~TestClass(){}
+
+    // class内の演算子オーバーロード
+    TestClass operator +(TestClass tc){
+        return TestClass{ a + tc.a };
+    }
+    TestClass operator ++(){
+        ++a;
+        return TestClass{ a };
+    }
+    TestClass operator ++(int invalid_num){
+        auto temp = a;
+        ++a;
+        return TestClass{ temp };
+    }
+};
+
+// test用main関数
+int main(){
+
+    TestClass tc1(10);
+    TestClass tc2(20);
+
+    // 演算子のtest
+    std::cout << tc1.a + tc2.a << std::endl;    // 30
+    std::cout << ++tc1.a << std::endl;          // 11
+    std::cout << tc1.a++ << std::endl;          // 11
+    std::cout << tc1.a << std::endl;            // 12
+
+    return 0;
+}
