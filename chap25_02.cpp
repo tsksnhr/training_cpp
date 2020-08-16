c7162e5b6d420c973ee5c5c25ccb59fc5164148f
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-08-17 00:34:37 +0900

    move a position of class-definition and made test-function

--- chap25_02.cpp
+++ chap25_02.cpp
@@ -1,6 +1,26 @@
 #include "all.h"
 
 //  template
+template <typename Array>
+struct my_array_iterator{
+
+    //  data member
+    Array &data;
+
+    //  constructor
+    //  データメンバに対する特別な文法を使用している
+    my_array_iterator(Array &data): data(data){
+
+    }
+
+    //  戻り値は配列の先頭要素へのリファレンス
+    //  Arrayがtemplate依存、referenceがネストされた型名なのでreferenceが必要
+    typename Array::reference operator *(){
+        return data[0];
+    }
+};
+
+//  template
 template <typename T, std::size_t N>
 struct my_array{
 
@@ -13,7 +33,7 @@ struct my_array{
     using const_reference = T const &;
     using data_size = std::size_t;
     //  イテレータに対するエイリアス
-    //  ここでエイリアスが実施できない・・・
+    //  my_array_iteratorが自作classなので、my_array_iteratorの定義か宣言がこのエイリアスより上に必要
     using iterator = my_array_iterator<my_array>;
 
     //  operator overload
@@ -29,26 +49,14 @@ struct my_array{
     }
 };
 
-//  template
-template <typename Array>
-struct my_array_iterator{
-
-    //  data member
-    Array &data;
-
-    //  constructor
-    my_array_iterator(Array &data): data(data){
-
-    }
-
-    //  戻り値は配列の先頭要素へのリファレンス
-    //  Arrayがtemplate依存、referenceがネストされた型名なのでreferenceが必要
-    typename Array::reference operator *(){
-        return data[0];
-    }
-};
-
 //  test
 int main(){
 
+    my_array<int, 3> a = {100,101,102};
+    my_array_iterator<my_array<int, 3>> a_iter(a);
+
+    auto iter = a.begin();
+    std::cout << *iter << std::endl;
+
+    return 0;
 }
